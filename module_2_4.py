8178991eb55075fe36852c46b7d48dc61a5d3019
Pavel-008 <viguropasha@gmail.com>
2024-08-23 08:38:54 +0500

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,19 @@
+
+nambers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,]
+primes = []
+not_primes = []
+for i in nambers:
+    is_prime = 0
+    for j in nambers:
+        if i % j == 0:
+            is_prime += 1
+            if is_prime > 2:
+                break
+        if i / j < 1:
+            break
+    if is_prime == 2:
+        primes.append(i)
+    if is_prime > 2:
+        not_primes.append(i)
+print(primes)
+print(not_primes)
